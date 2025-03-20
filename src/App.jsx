3768297e346d45e4a7bb04384666055f046c582c
Ilmanmdifa <ilmanmdifa63@gmail.com>
2025-03-20 14:46:56 +0700

    1 - adding css, init app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,22 @@
-import { useState } from "react";
-
-function binarySeatch(arr, target) {
-  let left = 0;
-  let right = arr.length - 1;
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
-  }
-  return -1;
-}
+import { Tabs } from "./components/Tabs";
+import { Header } from "./components/Header";
+import { TodoList } from "./components/TodoList";
+import { TodoInput } from "./components/TodoInput";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-
-  const example = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const target = 5;
-  console.log(binarySeatch(example, target));
-
-  const addTodo = () => {
-    if (inputValue.trim()) {
-      setTodos([...todos, inputValue]);
-      setInputValue(""); // Clear input after adding
-    }
-  };
-
+  const todos = [
+    { input: "Hello! Add your first todo!", complete: true },
+    { input: "Get the groceries!", complete: false },
+    { input: "Learn how to web design", complete: false },
+    { input: "Say hi to gran gran", complete: true },
+  ];
   return (
-    <div>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder="Add a todo"
-      />
-      <button onClick={addTodo}>Add</button>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Header />
+      <Tabs />
+      <TodoList />
+      <TodoInput />
+    </>
   );
 }
 
