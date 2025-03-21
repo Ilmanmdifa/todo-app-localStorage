f8ca4c3c14d05e303a409649137e0fb9fe683c6a
Ilmanmdifa <ilmanmdifa63@gmail.com>
2025-03-21 15:18:29 +0700

    2 - adding create function for the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,30 @@ import { Tabs } from "./components/Tabs";
 import { Header } from "./components/Header";
 import { TodoList } from "./components/TodoList";
 import { TodoInput } from "./components/TodoInput";
+import { useState } from "react";
 
 function App() {
-  const todos = [
+  // const todos = [
+  //   { input: "Hello! Add your first todo!", complete: true },
+  //   { input: "Get the groceries!", complete: false },
+  //   { input: "Learn how to web design", complete: false },
+  //   { input: "Say hi to gran gran", complete: true },
+  // ];
+  const [todos, setTodos] = useState([
     { input: "Hello! Add your first todo!", complete: true },
-    { input: "Get the groceries!", complete: false },
-    { input: "Learn how to web design", complete: false },
-    { input: "Say hi to gran gran", complete: true },
-  ];
+  ]);
+
+  function handleAddTodo(newTodo) {
+    const newTodoList = [...todos, { input: newTodo, complete: false }];
+    setTodos(newTodoList);
+  }
+
   return (
     <>
       <Header todos={todos} />
       <Tabs todos={todos} />
       <TodoList todos={todos} />
-      <TodoInput />
+      <TodoInput handleAddTodo={handleAddTodo} />
     </>
   );
 }
