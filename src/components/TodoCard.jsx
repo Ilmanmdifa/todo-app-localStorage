55af9b39233dcbd909b44ef4735b6afcb965f796
Ilmanmdifa <ilmanmdifa63@gmail.com>
2025-03-21 14:33:55 +0700

    1 - adding input and button with font awesome

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,13 +1,12 @@
 export function TodoCard(props) {
-  const { todoIndex, todos } = props;
-  const todo = todos[todoIndex];
-  console.log(todo);
+  const { todo } = props;
+
   return (
     <div className="card todo-item">
       <p>{todo.input}</p>
       <div className="todo-buttons">
+        <button disabled={todo.complete}>Done</button>
         <button>Delete</button>
-        <button>Done</button>
       </div>
     </div>
   );
